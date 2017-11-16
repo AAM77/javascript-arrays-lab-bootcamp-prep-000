e13f1359febe89f45ed19add2deeb119201b7974
AAM77 <adeel.a.mhd@gmail.com>
2017-11-16 18:43:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name);
   return kittens;
 }
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name) {
+  newKittensList = kittens.push(name);
+  return newKittensList;
+}
+
+function prependKitten(name) {
+  newKittensList = kittens.unshift(name);
+  return newKittensList;
+}
+
+function removeLastKitten() {
+  lessKittens = kittens.pop();
+  return lessKittens;
+}
+
+function removeFirstKitten() {
+  lessKittens = kittens.shift();
+  return lessKittens;
+}
